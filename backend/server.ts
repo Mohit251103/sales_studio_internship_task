55ee1a9b54fcd9290a65f54f45fc97deb30b4ba3
Mohit Negi <mnegi1433@gmail.com>
2025-03-12 19:45:31 +0530

    Backend deployment issue

--- backend/server.ts
+++ backend/server.ts
@@ -11,10 +11,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestIp.mw());
